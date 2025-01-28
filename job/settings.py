8479758c4af9467e8247ef7d35608ff8bf9317c6
Prabin Rijal <rijalprabin86@gmail.com>
2025-01-28 07:28:21 +0000

    add make changes in settings.py

--- job/settings.py
+++ job/settings.py
@@ -28,9 +28,9 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000, djobportal.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1:8000, swift-jobs.herokuapp.com']
 
 
 # Application definition
